ed2a42c2ae7e45f7400665b7839d9e57ca4d9c7d
r1zyh <kirill.ananiev2012@yandex.ru>
2024-09-19 22:53:43 +0300

    Добавит начальную разметку и опишет классы. Уберет лишнии комментарии из кода в taskmanager. Уберет лишние файлы.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,5 @@
 import Main from "../pages/main/ui/main";
 
-/*
-Допилить верстку
-Normalize CSS
-Избавиться от дефолтных стилей 
-Посмотреть в сторону SASS и типизированных scss.модулей
-Продумать лучше формат contacts и about в footer, а так же header с search-bar и nav-bar в header.
-Почекать TS config и настройки Lint
-Так как не умею попка писать back, для имитации работы с сервером будет JSON server.
-При типизации стоит закомментировать все паблик типы
-Добавить Routes для страничек
-
-*/ 
 
 function App() {
   return (
