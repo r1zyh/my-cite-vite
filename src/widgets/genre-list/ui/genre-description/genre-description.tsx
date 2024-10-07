de9c7fd81ceced516b186c2e5a25168893c8f91a
r1zyh <kirill.ananiev2012@yandex.ru>
2024-10-07 13:02:55 +0300

    небольшие доработки

--- src/widgets/genre-list/ui/genre-description/genre-description.tsx
+++ src/widgets/genre-list/ui/genre-description/genre-description.tsx
@@ -1,12 +1,16 @@
+import { Button } from "@/shared/ui/button/ui";
+
 export function GenreDescription(): JSX.Element {
   return (
-    <div className="genre__description">
+    <li className="genre__list--item">
+      <h2>SoulsLike</h2>
       <p className="genre__description--text">
         {" "}
         Soulslike (also spelled Souls-like) is a subgenre of action role-playing
         games known for high difficulty level and emphasis on environmental
         storytelling, typically in a dark fantasy setting.{" "}
       </p>
-    </div>
+      <Button text="Genre Page" />
+    </li>
   );
 }
--- src/widgets/genre-list/ui/genre-list-item/genre-list-item.tsx
+++ src/widgets/genre-list/ui/genre-list-item/genre-list-item.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/shared/ui/button/ui";
 import { GameList } from "@/widgets/games-list/ui";
+import { GenreDescription } from "../genre-description";
 
 export function GenreListItem(): JSX.Element {
   const soulsListLength = 4;
@@ -7,17 +8,20 @@ export function GenreListItem(): JSX.Element {
 
   return (
     <>
-      <li className="genre__list--item">
-        <h2>SoulsLike</h2>
-        <>
-          <GameList index={1} />
-          {soulsListLength > 3 ? (
-            <Button text="Show More" />
-          ) : (
-            <Button text="Genre Page" />
-          )}
-        </>
-      </li>
+      <div className="genre__list--inner">
+        <li className="genre__list--item front" >
+          <h2>SoulsLike</h2>
+          <>
+            <GameList index={1} />
+            {soulsListLength > 3 ? (
+              <Button text="Show More" />
+            ) : (
+              <Button text="Genre Page" />
+            )}
+          </>
+        </li>
+        <GenreDescription />
+      </div>
       <li className="genre__list--item">
         <h2>MOBA</h2>
         <GameList />
