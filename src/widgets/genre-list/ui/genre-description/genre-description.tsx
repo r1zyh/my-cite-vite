60efe6b764be947bd26c2c9d0732f08258864258
Kirill Ananyev <richeeq@gmail.com>
2024-10-09 18:25:45 +0300

    Добавит компонент Rating и логотип в Header.

--- src/widgets/genre-list/ui/genre-description/genre-description.tsx
+++ src/widgets/genre-list/ui/genre-description/genre-description.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/shared/ui/button/ui";
+import Typography from "@mui/material/Typography";
 
 type TGenreDescriptionProps = {
   title: string;
@@ -14,7 +15,7 @@ export function GenreDescription({
   return (
     <div className="genre__item--back">
       <h2 className="genre__title" onClick={click}>{title}</h2>
-      <p className="genre__description--text"> {description} </p>
+      <Typography className="genre__description--text"> {description} </Typography>
       <Button text="Genre Page" />
     </div>
   ) 
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -2,11 +2,34 @@ import { Link } from "react-router-dom";
 import { AppRoute } from "@/shared/const";
 
 export function Header(): JSX.Element {
+
+  //временное лого, в будущем сделаю нормальную SVG
+
   return (
     <header className="header">
       <div className="logo">
         <Link to={AppRoute.Main}>
-          <img src="" alt="my hopefully future self made logo" />
+          <svg
+            width="64"
+            height="64"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ color: "teal", borderRadius: "20%" }}
+          >
+            <rect width="64" height="64" fill="#FAA860" />
+            <text
+              x="50%"
+              y="75%"
+              dominantBaseline="middle"
+              textAnchor="middle"
+              fontSize="46"
+              fill="#FFFFFF"
+              fontFamily="Roboto, sans-serif"
+              fontWeight="bold"
+              color="white"
+            >
+              КА
+            </text>
+          </svg>
         </Link>
       </div>
     </header>
