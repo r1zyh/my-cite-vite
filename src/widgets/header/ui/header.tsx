060355497b56fcf4d941b39257a43c6315adc39c
Kirill Ananyev <richeeq@gmail.com>
2024-10-10 19:06:05 +0300

    Начнет перенос проекта на SCSS модули.

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,31 +1,26 @@
 import { Link } from "react-router-dom";
 import { AppRoute } from "@/shared/const";
+import styles from './header.module.scss';
 
 export function Header(): JSX.Element {
 
-  //временное лого, в будущем сделаю нормальную SVG
-
   return (
-    <header className="header">
-      <div className="logo">
+    <header className={styles.header}>
+      <div className={styles.header__logo}>
         <Link to={AppRoute.Main}>
           <svg
+            className={styles.header__svg}
             width="64"
             height="64"
             xmlns="http://www.w3.org/2000/svg"
-            style={{ color: "teal", borderRadius: "20%" }}
           >
             <rect width="64" height="64" fill="#FAA860" />
             <text
+              className={styles.header__text}
               x="50%"
               y="75%"
               dominantBaseline="middle"
               textAnchor="middle"
-              fontSize="46"
-              fill="#FFFFFF"
-              fontFamily="Roboto, sans-serif"
-              fontWeight="bold"
-              color="white"
             >
               КА
             </text>
