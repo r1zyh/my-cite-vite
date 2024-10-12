4fb3856f60fa3244adf30b83060e3d947c9ca6ee
Kirill Ananyev <richeeq@gmail.com>
2024-10-12 15:55:30 +0300

    prettier fix

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,10 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import eslintReact from "eslint-plugin-react";
-import eslintReactHooks from "eslint-plugin-react-hooks"
+import eslintReactHooks from "eslint-plugin-react-hooks";
 import eslintReactRefresh from "eslint-plugin-react-refresh";
 import prettierPlugin from "eslint-plugin-prettier";
 import eslintConfigPrettier from "eslint-config-prettier";
-
-
 
 export default [
   // Базовая конфигурация JS и TypeScript
