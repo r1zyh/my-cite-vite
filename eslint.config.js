7de030d060c6bd519cccc699687ac6d1e751ff3c
Kirill Ananyev <richeeq@gmail.com>
2024-10-12 15:54:21 +0300

    Попытка пофиксить eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,68 +2,78 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import eslintReact from "eslint-plugin-react";
-import eslintReactHooks from "eslint-plugin-react-hooks";
+import eslintReactHooks from "eslint-plugin-react-hooks"
 import eslintReactRefresh from "eslint-plugin-react-refresh";
 import prettierPlugin from "eslint-plugin-prettier";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-export default {
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  plugins: {
-    "@typescript-eslint": tseslint,
-    react: eslintReact,
-    "react-hooks": eslintReactHooks,
-    "react-refresh": eslintReactRefresh,
-    prettier: prettierPlugin,
-  },
-  ignores: [
-    "dist",
-    "node_modules",
-    "coverage",
-    "eslint.config.js",
-    "package-lock.json",
-    "package.json",
-    "git",
-  ],
-  ...js.configs.recommended,
+
+
+export default [
+  // Базовая конфигурация JS и TypeScript
+  js.configs.recommended,
   ...tseslint.configs.recommended,
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.node,
-      ...globals.es2020,
-    },
-    parserOptions: {
-      project: ["tsconfig.json", "tsconfig.node.json", "tsconfig.app.json"],
-    },
-  },
-  overrides: [
-    {
-      files: ["**/*.{ts,tsx}"],
-      rules: {
-        ...prettierPlugin.configs.recommended.rules,
-        ...eslintConfigPrettier.rules,
-        "react-refresh/only-export-components": [
-          "warn",
-          { allowConstantExport: true },
-        ],
-        "prefer-const": "error",
-        "react/jsx-curly-brace-presence": [
-          "warn",
-          { props: "never", children: "never" },
-        ],
-        "react/function-component-definition": [
-          "warn",
-          { namedComponents: "arrow-function" },
-        ],
-        "react/self-closing-comp": ["error", { component: true, html: true }],
-        "max-lines": ["warn", { max: 124 }],
-        "max-params": ["error", 3],
+
+  // Настройки React
+  {
+    settings: {
+      react: {
+        version: "detect",
       },
     },
-  ],
-};
+  },
+
+  // Плагины
+  {
+    plugins: {
+      react: eslintReact,
+      "react-hooks": eslintReactHooks,
+      "react-refresh": eslintReactRefresh,
+      prettier: prettierPlugin,
+    },
+  },
+
+  // Игнорируемые файлы и каталоги
+  {
+    ignores: ["dist", "node_modules", "coverage", "eslint.config.js"],
+  },
+
+  // Опции языка
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        project: ["tsconfig.json", "tsconfig.node.json", "tsconfig.app.json"],
+      },
+    },
+  },
+
+  // Переопределения для файлов TypeScript
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "prefer-const": "error",
+      "react/jsx-curly-brace-presence": [
+        "warn",
+        { props: "never", children: "never" },
+      ],
+      "react/function-component-definition": [
+        "warn",
+        { namedComponents: "arrow-function" },
+      ],
+      "react/self-closing-comp": ["error", { component: true, html: true }],
+      "max-lines": ["warn", { max: 124 }],
+      "max-params": ["error", 3],
+    },
+  },
+];
--- src/widgets/review-section/review-section.tsx
+++ src/widgets/review-section/review-section.tsx
@@ -2,5 +2,9 @@ import { ReviewList } from './ui/review-list';
 import { ReviewSectionWrapper } from './ui/review-section-wrapper';
 
 export const ReviewSection = (): JSX.Element => {
-  return <ReviewSectionWrapper>{<ReviewList />}</ReviewSectionWrapper>;
+  return (
+    <ReviewSectionWrapper>
+      <ReviewList />
+    </ReviewSectionWrapper>
+  );
 };
