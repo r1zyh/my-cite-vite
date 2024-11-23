021b4d559ebadcd26863e48e4c9355b6a48f4500
r1zyh <kirill.ananiev2012@yandex.ru>
2024-11-23 21:25:27 +0300

    Вынесет список игр в json файл

--- src/widgets/genre-list/ui/genre-list-item/genre-list-item.tsx
+++ src/widgets/genre-list/ui/genre-list-item/genre-list-item.tsx
@@ -4,51 +4,18 @@ import { GenreDescription } from '../genre-description';
 import { useState } from 'react';
 import { TGame } from '@/widgets/games-list/ui/types';
 import styles from './genre-list-item.module.scss';
+import data from '@/assets/data/games.json';
 
 const gameListLength = 4;
 
-const genres = [
-  {
-    title: 'SoulsLike',
-    description:
-      'Soulslike (also spelled Souls-like) is a subgenre of action role-playing games known for high difficulty level and emphasis on environmental storytelling, typically in a dark fantasy setting.',
-    games: [
-      { title: 'Dark Souls 1', image: '/src/assets/ds1.png' },
-      { title: 'Dark Souls 3', image: '/src/assets/ds3resizedV2.jpg' },
-      { title: 'Elden Ring', image: '/src/assets/eldenring.png' },
-    ] as TGame[],
-  },
-  {
-    title: 'MOBA',
-    description:
-      'Multiplayer online battle arena (MOBA) is a subgenre of strategy video games in which two teams of players compete against each other on a predefined battlefield. Some text Some text Some text',
-    games: [
-      { title: 'Dota 2', image: '/src/assets/dota2.png' },
-      { title: 'League of Legends', image: '/src/assets/lol.png' },
-      { title: 'Deadlock', image: '/src/assets/deadlock.png' },
-    ] as TGame[],
-  },
-  {
-    title: 'Shooter',
-    description:
-      "Shooter video games or shooters are a subgenre of action video games where the focus is on the defeat of the character's enemies using ranged weapons given to the player. Some text Some text Some text",
-    games: [
-      { title: 'CS 2', image: '/src/assets/cs2.png' },
-      { title: 'Valorant', image: '/src/assets/valorant.png' },
-      { title: 'Call of Duty: Warzone', image: '/src/assets/warzone.png' },
-    ] as TGame[],
-  },
-  {
-    title: 'Shooter',
-    description:
-      "Shooter video games or shooters are a subgenre of action video games where the focus is on the defeat of the character's enemies using ranged weapons given to the player. Some text Some text Some text",
-    games: [
-      { title: 'CS 2', image: '/src/assets/cs2.png' },
-      { title: 'Valorant', image: '/src/assets/valorant.png' },
-      { title: 'Call of Duty: Warzone', image: '/src/assets/warzone.png' },
-    ] as TGame[],
-  },
-];
+type TGenres = {
+  title: string;
+  description: string;
+  games: TGame[];
+};
+
+const genres = data as TGenres[];
+
 export const GenreListItem = (): JSX.Element => {
   const [descriptionToggle, setDescriptionToggle] = useState(Array(genres.length).fill(false));
 
