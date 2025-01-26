8a25dd868688f89c422b81ff067c7955e7d7168f
Kirill Ananyev <richeeq@gmail.com>
2025-01-26 20:02:27 +0300

    Добавит начальную верстку для страницы genre page

--- src/pages/genre/ui/genre.tsx
+++ src/pages/genre/ui/genre.tsx
@@ -3,10 +3,127 @@ import { Header } from '@/widgets/header/ui';
 
 export const Genre = (): JSX.Element => {
   return (
-    <div className="wrapper">
+    <>
       <Header />
-      <div>Some Genre Page template</div>
+      <main>
+        <div className="genre__page">
+          <div className="games__grid">
+            <ul className="games__list">
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+              <li className="games__list--item">
+                <h3 className="title">Item 1</h3>
+                <img src="#" alt="game__preview" />
+                <p className="game__studio">Bandai Namco</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
       <Footer />
-    </div>
+    </>
   );
 };
